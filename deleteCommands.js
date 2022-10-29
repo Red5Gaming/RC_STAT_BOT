a2e6f89dcce0b4d3123bd0453612bb4bcd7399b3
RedGaming <52105457+Red5Gaming@users.noreply.github.com>
2022-10-29 18:22:18 +0200

    Update deleteCommands.js

--- deleteCommands.js
+++ deleteCommands.js
@@ -5,15 +5,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 // ...
 
-// for guild-based commands
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1033004803590193186'))
+// for guild-based commands.
+rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
     .then(() => console.log('Successfully deleted guild command'))
     .catch(console.error);
-
-
-
-
-// for global commands
-// rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-//     .then(() => console.log('Successfully deleted application command'))
-//     .catch(console.error);
