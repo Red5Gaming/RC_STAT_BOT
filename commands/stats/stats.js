6a12cbc58679d4db5195d9ea71fa017eae5fb9cc
Red5Gaming <nicompf05@gmail.com>
2022-10-29 17:10:37 +0200

    stas.js now working.

--- commands/stats/stats.js
+++ commands/stats/stats.js
@@ -1,9 +1,9 @@
 const {SlashCommandBuilder, EmbedBuilder, Embed, ButtonStyle} = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 const Pagination = require('customizable-discordjs-pagination');
-const superagent = require('superagent');
-const config = require('../../config.json');
+
 const req = require('../../utils/requestHandler.js')
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('stats')
@@ -26,8 +26,22 @@ module.exports = {
         if(interaction.channel.id != '1004081020062138370') return interaction.reply({content: 'Please use this command in <#1004081020062138370>', ephemeral: true});
 
 
+
+
+
+        await interaction.deferReply();
+
         const name = interaction.options.getString('name');
         const platform = interaction.options.getString('platform');
+
+        // edit platform, so PC = PC, PSN = Playstation, XBL = Xbox, SWITCH = Nintendo Switch
+        let platformEdit = platform;
+        if(platform == 'uplay') platformEdit = 'PC';
+        if(platform == 'psn') platformEdit = 'Playstation';
+        if(platform == 'xbl') platformEdit = 'Xbox';
+        if(platform == 'switch') platformEdit = 'Nintendo Switch';
+
+
         console.log(name);
         console.log(platform);
         try {
@@ -40,6 +54,17 @@ module.exports = {
                     return "0";
                 }
             }
+
+            function addStats(...args) {
+              // the args are the name of stats, add all their values together and return the total
+                let total = 0;
+                for (let i = 0; i < args.length; i++) {
+                    total += Number(getStat(args[i]));
+                }
+                return total;
+            }
+
+            console.log("Total" + addStats('performanceEmoteGamemode.gamemodeid.Exotic', 'performanceEmoteGamemode.gamemodeid.QuickMatch', 'performanceEmoteGamemode.gamemodeid.Ranked'))
 
 
             // times
@@ -58,7 +83,7 @@ module.exports = {
                  dodgesinexotic: getStat('performanceDodgeGamemode.gamemodeid.Exotic'),
                  dodgesinquickmatch: getStat('performanceDodgeGamemode.gamemodeid.QuickMatch'),
                  dodgesinranked: getStat('performanceDodgeGamemode.gamemodeid.Ranked'),
-                 calculatedtotaldodges: (Number(this.dodgesinexotic)) + (Number(this.dodgesinquickmatch)) + (Number(this.dodgesinranked))
+                 calculatedtotaldodges: (Number(getStat('performanceDodgeGamemode.gamemodeid.Exotic'))) + (Number(getStat('performanceDodgeGamemode.gamemodeid.QuickMatch'))) + (Number(getStat('performanceDodgeGamemode.gamemodeid.Ranked')))
             }
 
             // emotes
@@ -67,7 +92,7 @@ module.exports = {
                  emotesinexotic: getStat('performanceEmoteGamemode.gamemodeid.Exotic'),
                  emotesinquickmatch: getStat('performanceEmoteGamemode.gamemodeid.QuickMatch'),
                  emotesinranked: getStat('performanceEmoteGamemode.gamemodeid.Ranked'),
-                 calculatedemotes: (Number(this.emotesinexotic)) + (Number(this.emotesinquickmatch)) + (Number(this.emotesinranked))
+                 calculatedemotes: (Number(getStat('performanceEmoteGamemode.gamemodeid.Exotic'))) + (Number(getStat('performanceEmoteGamemode.gamemodeid.QuickMatch'))) + (Number(getStat('performanceEmoteGamemode.gamemodeid.Ranked')))
             }
 
             // gates
@@ -76,8 +101,10 @@ module.exports = {
                  gatesinexotic: getStat('performanceGatesGamemode.gamemodeid.Exotic'),
                  gatesinquickmatch: getStat('performanceGatesGamemode.gamemodeid.QuickMatch'),
                  gatesinranked: getStat('performanceGatesGamemode.gamemodeid.Ranked'),
-                 calculatedgates: (Number(this.gatesinexotic)) + (Number(this.gatesinquickmatch)) + (Number(this.gatesinranked))
+                 calculatedgates: addStats('performanceGatesGamemode.gamemodeid.Exotic', 'performanceGatesGamemode.gamemodeid.QuickMatch', 'performanceGatesGamemode.gamemodeid.Ranked')
             }
+
+            console.log(gates.calculatedgates)
 
             // all goals
             let allGoals = {
@@ -90,7 +117,7 @@ module.exports = {
                  exotic1ptgoals: getStat('performance1ptGoalGamemode.gamemodeid.Exotic'),
                  quickmatch1ptgoals: getStat('performance1ptGoalGamemode.gamemodeid.QuickMatch'),
                  ranked1ptgoals: getStat('performance1ptGoalGamemode.gamemodeid.Ranked'),
-                 calculated1ptgoals: (Number(this.exotic1ptgoals)) + (Number(this.quickmatch1ptgoals)) + (Number(this.ranked1ptgoals))
+                 calculated1ptgoals: addStats('performance1ptGoalGamemode.gamemodeid.Exotic', 'performance1ptGoalGamemode.gamemodeid.QuickMatch', 'performance1ptGoalGamemode.gamemodeid.Ranked')
             }
 
             // 3pt goals
@@ -99,7 +126,7 @@ module.exports = {
                  exotic3ptgoals: getStat('performance3ptGoalGamemode.gamemodeid.Exotic'),
                  quickmatch3ptgoals: getStat('performance3ptGoalGamemode.gamemodeid.QuickMatch'),
                  ranked3ptgoals: getStat('performance3ptGoalGamemode.gamemodeid.Ranked'),
-                 calculated3ptgoals: (Number(this.exotic3ptgoals)) + (Number(this.quickmatch3ptgoals)) + (Number(this.ranked3ptgoals))
+                 calculated3ptgoals: addStats('performance3ptGoalGamemode.gamemodeid.Exotic', 'performance3ptGoalGamemode.gamemodeid.QuickMatch', 'performance3ptGoalGamemode.gamemodeid.Ranked')
             }
 
             // 5pt goals
@@ -108,7 +135,7 @@ module.exports = {
                  exotic5ptgoals: getStat('performance5ptGoalGamemode.gamemodeid.Exotic'),
                  quickmatch5ptgoals: getStat('performance5ptGoalGamemode.gamemodeid.QuickMatch'),
                  ranked5ptgoals: getStat('performance5ptGoalGamemode.gamemodeid.Ranked'),
-                 calculated5ptgoals: (Number(this.exotic5ptgoals)) + (Number(this.quickmatch5ptgoals)) + (Number(this.ranked5ptgoals))
+                 calculated5ptgoals: addStats('performance5ptGoalGamemode.gamemodeid.Exotic', 'performance5ptGoalGamemode.gamemodeid.QuickMatch', 'performance5ptGoalGamemode.gamemodeid.Ranked')
             }
 
             let ptPercentages = {
@@ -123,7 +150,7 @@ module.exports = {
                  mategrabsinexotic: getStat('performanceGrabGamemode.gamemodeid.Exotic'),
                  mategrabsinquickmatch: getStat('performanceGrabGamemode.gamemodeid.QuickMatch'),
                  mategrabsinranked: getStat('performanceGrabGamemode.gamemodeid.Ranked'),
-                 calculatedmategrabs: (Number(this.mategrabsinexotic)) + (Number(this.mategrabsinquickmatch)) + (Number(this.mategrabsinranked))
+                 calculatedmategrabs: addStats('performanceGrabGamemode.gamemodeid.Exotic', 'performanceGrabGamemode.gamemodeid.QuickMatch', 'performanceGrabGamemode.gamemodeid.Ranked')
             }
 
             // passes
@@ -132,7 +159,7 @@ module.exports = {
                  quickmatchPasses: getStat('performancePassGamemode.gamemodeid.QuickMatch'),
                  rankedPasses: getStat('performancePassGamemode.gamemodeid.Ranked'),
                  exoticPasses: getStat('performancePassGamemode.gamemodeid.Exotic'),
-                 calculatedPasses: (Number(this.quickmatchPasses)) + (Number(this.rankedPasses)) + (Number(this.exoticPasses))
+                 calculatedPasses: addStats('performancePassGamemode.gamemodeid.Exotic', 'performancePassGamemode.gamemodeid.QuickMatch', 'performancePassGamemode.gamemodeid.Ranked')
             }
 
             // stuns
@@ -141,7 +168,7 @@ module.exports = {
              quickmatchstuns: getStat('performanceStunGamemode.gamemodeid.QuickMatch'),
              rankedstuns: getStat('performanceStunGamemode.gamemodeid.Ranked'),
              exoticstuns: getStat('performanceStunGamemode.gamemodeid.Exotic'),
-             calculatedstuns: (Number(this.quickmatchstuns)) + (Number(this.rankedstuns)) + (Number(this.exoticstuns))
+             calculatedstuns: addStats('performanceStunGamemode.gamemodeid.Exotic', 'performanceStunGamemode.gamemodeid.QuickMatch', 'performanceStunGamemode.gamemodeid.Ranked')
             }
 
             // tackles
@@ -150,7 +177,7 @@ module.exports = {
              quickmatchtackles: getStat('performanceTacklesGamemode.gamemodeid.QuickMatch'),
              rankedtackles: getStat('performanceTacklesGamemode.gamemodeid.Ranked'),
              exotictackles: getStat('performanceTacklesGamemode.gamemodeid.Exotic'),
-             calculatedtackles: (Number(this.quickmatchtackles)) + (Number(this.rankedtackles)) + (Number(this.exotictackles))
+             calculatedtackles: addStats('performanceTacklesGamemode.gamemodeid.Exotic', 'performanceTacklesGamemode.gamemodeid.QuickMatch', 'performanceTacklesGamemode.gamemodeid.Ranked')
             }
 
             // distances
@@ -159,7 +186,7 @@ module.exports = {
                  exoticdistance: Number(getStat('performanceDistanceGamemode.gamemodeid.Exotic') / 1000).toFixed(2) + " km",
                  quickmatchdistance: (Number(getStat('performanceDistanceGamemode.gamemodeid.QuickMatch')) / 1000).toFixed(2) + " km",
                  rankeddistance: (Number(getStat('performanceDistanceGamemode.gamemodeid.Ranked')) / 1000).toFixed(2) + " km", //(rankeddistance == 'NaN' ? "0" : (rankeddistance / 1000).toFixed(2)) + " km",
-                 calculateddistance: ((Number(getStat('performanceDistanceGamemode.gamemodeid.Exotic')) + (Number(getStat('performanceDistanceGamemode.gamemodeid.QuickMatch')) + Number(getStat('performanceDistanceGamemode.gamemodeid.Ranked')))) / 1000).toFixed(2) + " km"
+                 calculateddistance:  (addStats('performanceDistanceGamemode.gamemodeid.Exotic', 'performanceDistanceGamemode.gamemodeid.QuickMatch', 'performanceDistanceGamemode.gamemodeid.Ranked') / 1000).toFixed(2) + " km"
             }
 
             // 1 / 0 stats
@@ -167,7 +194,7 @@ module.exports = {
                  onetozeroexotic: getStat('progressionEndOfMatchEnemyScore.gamemodeid.Exotic.selfscore.1.otherscore.0'),
                  onetozeroquickmatch: getStat('progressionEndOfMatchEnemyScore.gamemodeid.QuickMatch.selfscore.1.otherscore.0'),
                  onetozeroranked: getStat('progressionEndOfMatchEnemyScore.gamemodeid.Ranked.selfscore.1.otherscore.0'),
-                 totalonetozero: Number(this.onetozeroexotic) + Number(this.onetozeroquickmatch) + Number(this.onetozeroranked)
+                 totalonetozero: addStats('progressionEndOfMatchEnemyScore.gamemodeid.Exotic.selfscore.1.otherscore.0', 'progressionEndOfMatchEnemyScore.gamemodeid.QuickMatch.selfscore.1.otherscore.0', 'progressionEndOfMatchEnemyScore.gamemodeid.Ranked.selfscore.1.otherscore.0'),
             }
 
             // map specific data
@@ -211,11 +238,20 @@ module.exports = {
                  totalwins: getStat('MatchResult.endreason.Win'),
                  toaldraws: getStat('MatchResult.endreason.Draw'),
                  totalLost: getStat('MatchResult.endreason.Lost'),
-                 winpercentage: (Number(this.totalwins) / Number(this.totalmatches) * 100).toFixed(2).toString() + "%",
-                 losspercentage: (Number(this.totalLost) / Number(this.totalmatches) * 100).toFixed(2).toString() + "%",
-                 drawpercentage: (Number(this.toaldraws) / Number(this.totalmatches) * 100).toFixed(2).toString() + "%",
-                 calculatedlosses: (Number(this.totalmatches) - Number(this.totalwins) - Number(this.toaldraws)).toString()
+                 winpercentage: Number((Number(getStat('MatchResult.endreason.Win')) / Number(getStat('MatchPlayed')) * 100).toFixed(2)) + "%",
+                 losspercentage: (Number(getStat('MatchResult.endreason.Lost')) / Number(getStat('MatchPlayed')) * 100).toFixed(2).toString() + "%",
+                 drawpercentage: (Number(getStat('MatchResult.endreason.Draw')) / Number(getStat('MatchPlayed')) * 100).toFixed(2).toString() + "%",
+                 calculatedlosses: (Number(getStat('MatchPlayed')) - Number(getStat('MatchResult.endreason.Win')) - Number(getStat('MatchResult.endreason.Draw'))).toString()
             }
+
+
+
+//             console.log(typeof matchesStats.winpercentage)
+// console.log(matchesStats.winpercentage)
+
+
+
+
 
             // exotic outcomes
             let exoticOutcomes = {
@@ -262,7 +298,6 @@ module.exports = {
             // every page is 3x4 fields
             const page1 = new EmbedBuilder()
                 .setTitle('Important Stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields({name: "MMR", value: mmr, inline: true},
                     {name: "Total Fans", value: fans.totalFans, inline: true},
                     {name: "Total Matches", value: matchesStats.totalmatches, inline: true},
@@ -311,7 +346,6 @@ module.exports = {
                 .setColor('#FF1653')
             const page2 = new EmbedBuilder()
                 .setTitle('Ranked Stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Ranked Wins", value: rankedOutcomes.rankedDraws, inline: true},
                     {name: "Ranked Draws", value: rankedOutcomes.rankedDraws, inline: true},
@@ -335,7 +369,6 @@ module.exports = {
                 )
             const page3 = new EmbedBuilder()
                 .setTitle('Quickmatch Stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "QM Wins", value: quickmatchOutcomes.quickmatchWins, inline: true},
                     {name: "QM draws", value: quickmatchOutcomes.quickmatchDraws, inline: true},
@@ -359,7 +392,6 @@ module.exports = {
                 )
             const page4 = new EmbedBuilder()
                 .setTitle('Exotic Stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Exotic Wins", value: exoticOutcomes.exoticWins, inline: true},
                     {name: "Exotic draws", value: exoticOutcomes.exoticDraws, inline: true},
@@ -402,7 +434,6 @@ module.exports = {
 
             const page5 = new EmbedBuilder()
                 .setTitle('Times Played')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {
                         name: "Calculated time",
@@ -432,7 +463,6 @@ module.exports = {
                 )
             const page6 = new EmbedBuilder()
                 .setTitle('Distances Travelled')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {
                         name: "Total distance travelled",
@@ -463,7 +493,6 @@ module.exports = {
                 )
             const page7 = new EmbedBuilder()
                 .setTitle('Grabs, Dodges, Tackles, Stuns, Emotes')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Total grabs", value: grabs.mategrabs.toString(), inline: true},
                     {
@@ -490,7 +519,6 @@ module.exports = {
                 )
             const page8 = new EmbedBuilder()
                 .setTitle('Goals, Gates, Percentages')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Total goals", value: allGoals.reportedgoals.toString(), inline: true},
                     {name: "Calculated 1pt goals", value: oneptgoals.calculated1ptgoals.toString(), inline: true},
@@ -507,7 +535,6 @@ module.exports = {
                 )
             const page9 = new EmbedBuilder()
                 .setTitle('Misc Stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Total 1/0 wins", value: onezerostats.totalonetozero.toString(), inline: true},
                     {name: "Total Matches won in 60 seconds", value: minutewins.minuteWinQuickmatch.toString(), inline: true},
@@ -517,7 +544,6 @@ module.exports = {
                 )
             const page10 = new EmbedBuilder()
                 .setTitle('Map stats')
-                .setDescription('Here are the stats for ' + "***" + name + "***" + " on " + platform)
                 .addFields(
                     {name: "Total games played in Arena 8", value: mapstats.arenaeightPlayed, inline: true},
                     {name: "Total games played in Acapulco", value: mapstats.acapulcoPlayed, inline: true},
@@ -536,7 +562,17 @@ module.exports = {
                     {name: "Total games played in Staten Island", value: mapstats.statenislandPlayed, inline: true},
                     {name: "Total games played in Venice Beach", value: mapstats.venicebeachPlayed, inline: true},
                 )
+
+
             let pages = [page0, page1, page2, page3, page4, page5, page6, page7, page8, page9, page10]
+
+            pages.forEach(page => {
+                page.setColor('#FF1653')
+                page.setDescription('Here are the stats for ' + "***" + name + "***" + " on " + "***" + platformEdit + "***")
+            })
+
+
+
             const buttons = [
                 {label: 'first', emoji: '⏪', style: ButtonStyle.Secondary},
                 {label: 'Previous', emoji: '⬅', style: ButtonStyle.Danger},
